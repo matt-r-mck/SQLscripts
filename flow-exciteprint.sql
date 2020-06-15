1a4af5d7e716a9d5ac1b73b1e1d08419089ef15c
max-student <max-student@maxtrain.com>
2020-06-15 09:44:43 -0400

    Created Procedure:

--- flow-exciteprint.sql
+++ flow-exciteprint.sql
@@ -11,9 +11,9 @@ begin
 end
 print concat('The Total is ', str(@TOTAL));
 
--- even excite function  */
+-- even excite function  
 
-
+create or alter procedure FACTORIAL
 declare @NUMBER int = 1;
 declare @TOTAL int = 0;
 declare @FACT int = 4;
@@ -25,7 +25,7 @@ begin
 end
 print concat(@FACT, '! = ', @TOTAL)
 
-/*
+
 if 1+5=0 begin
 	print 'Hello World'
 	print 'The End' --executed if true
@@ -34,3 +34,29 @@ else begin
 	print 'nope'--executed if false
 end
 */
+
+create or alter procedure FACTORIAL
+	@NBR int = null
+as
+begin
+	if @NBR is null begin
+		print 'ERROR: A positive integer is required.';
+		return;
+	end
+	if @NBR < 1 begin
+		print 'ERROR: Parameter must be >= 1'
+		return;
+	end
+	--assume we have a good parameter
+	declare @FACT int = 1;
+	declare @aNBR int = @NBR
+	while @aNBR > 1 begin
+		set @FACT = @FACT * @aNBR
+		set @aNBR = @aNBR - 1
+	end
+	print 'The factorial of ' + trim(str(@NBR)) + ' is ' + trim(str(@fact));
+	--select 
+end
+go
+exec factorial @nbr = 6
+go
--- sproc-cursor.sql
+++ sproc-cursor.sql
@@ -1,3 +1,4 @@
+/*
 declare curSTUDENT cursor for --scroll cursor#
 	select Firstname, Lastname, gpa, sat from student
 	order by Lastname; --cursor searches columns in table
@@ -9,16 +10,42 @@ declare @firstname varchar(30), @lastname varchar (30), @gpa float, @sat int;
 fetch next from curSTUDENT		--allows for first, etc#
 	into @firstname, @lastname, @gpa, @sat --scan first row
 
-while @@fetch_status = 0 --while statement is true
+while @@fetch_status = 0 --while there is a next row
 begin
 print concat(@lastname, ', ', @firstname, ', SAT is ', str(@sat));
-	if @sat < 1000
+	if @sat < 1000 --checking to see if there are ANT students with an sat >1000
 		break;
 	
 	fetch next from curSTUDENT
 		into @firstname, @lastname, @gpa, @sat; --scan consecutive rows
 end
 	
+close curSTUDENT;
+
+deallocate curSTUDENT;
+*/
+
+declare curSTUDENT cursor for
+	select Firstname, Lastname, gpa, sat
+		from student;
+
+		open curSTUDENT;
+
+declare @firstname varchar(30), @lastname varchar(30), @gpa float, @sat int;
+
+fetch next from curSTUDENT
+	into @firstname, @lastname, @gpa, @sat;
+
+while @@FETCH_STATUS = 0
+begin
+print concat(@lastname, ', ', @firstname, ' - SAT score is ', str(@sat));
+	if @sat <1000
+		break;
+	fetch next from curSTUDENT
+		into  @firstname, @lastname, @gpa, @sat;
+
+end
+
 close curSTUDENT;
 
 deallocate curSTUDENT;
